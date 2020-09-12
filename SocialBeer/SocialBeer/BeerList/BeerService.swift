4b0c2a6ec42beaacb5527ca9a5abba1341586da4
Jan-Erik Lorfeo <lorfeo@adesso-mobile.de>
2020-09-12 21:27:38 +0200

    api-connect: Implement service that connects between view Model and repository

--- SocialBeer/SocialBeer/BeerList/BeerService.swift
+++ SocialBeer/SocialBeer/BeerList/BeerService.swift
@@ -6,4 +6,21 @@
 //  Copyright © 2020 Lorfeo, Jan-Erik. All rights reserved.
 //
 
+import Combine
 import Foundation
+
+struct BeerService {
+    var repository: BeerRepository
+
+    var beerPublisher: AnyPublisher<[Beer], Error> {
+        repository.$currentData
+            .receive(on: RunLoop.main)
+            .map { $0 }
+            .decode(type: Array<Beer>.self, decoder: JSONDecoder())
+            .eraseToAnyPublisher()
+    }
+
+    func fetchBeers() {
+        repository.read()
+    }
+}
