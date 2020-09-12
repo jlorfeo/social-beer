39e28a74b8ab763939389b482ad71c8f7d6ada38
Jan-Erik Lorfeo <lorfeo@adesso-mobile.de>
2020-09-12 13:30:25 +0200

    ui-mock: Define searchbar

--- SocialBeer/SocialBeer/Views/SearchBar.swift
+++ SocialBeer/SocialBeer/Views/SearchBar.swift
@@ -6,4 +6,40 @@
 //  Copyright © 2020 Lorfeo, Jan-Erik. All rights reserved.
 //
 
-import Foundation
+import SwiftUI
+
+struct SearchBar: UIViewRepresentable {
+
+    @Binding var text: String
+    var placeholder: String
+
+    class Coordinator: NSObject, UISearchBarDelegate {
+
+        @Binding var text: String
+
+        init(text: Binding<String>) {
+            _text = text
+        }
+
+        func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+            text = searchText
+        }
+    }
+
+    func makeCoordinator() -> SearchBar.Coordinator {
+        return Coordinator(text: $text)
+    }
+
+    func makeUIView(context: UIViewRepresentableContext<SearchBar>) -> UISearchBar {
+        let searchBar = UISearchBar(frame: .zero)
+        searchBar.placeholder = placeholder
+        searchBar.delegate = context.coordinator
+        searchBar.searchBarStyle = .minimal
+        searchBar.autocapitalizationType = .none
+        return searchBar
+    }
+
+    func updateUIView(_ uiView: UISearchBar, context: UIViewRepresentableContext<SearchBar>) {
+        uiView.text = text
+    }
+}
