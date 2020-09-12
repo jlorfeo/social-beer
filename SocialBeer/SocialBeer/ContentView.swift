9d83e813223fb32818e0df363e4698c35e245f43
Jan-Erik Lorfeo <lorfeo@adesso-mobile.de>
2020-09-12 12:27:43 +0200

    ui-mock: Setup project

---
+++ SocialBeer/SocialBeer/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SocialBeer
+//
+//  Created by Lorfeo, Jan-Erik on 10.09.20.
+//  Copyright © 2020 Lorfeo, Jan-Erik. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
