790c8e4fc5231d4b38461dd59582a041cd2a016e
Jan-Erik Lorfeo <lorfeo@adesso-mobile.de>
2020-09-12 21:27:10 +0200

    api-connect: Build repository to connect to api

--- SocialBeer/SocialBeer/BeerList/BeerRepository.swift
+++ SocialBeer/SocialBeer/BeerList/BeerRepository.swift
@@ -6,4 +6,48 @@
 //  Copyright © 2020 Lorfeo, Jan-Erik. All rights reserved.
 //
 
+import Combine
 import Foundation
+
+class BeerRepository {
+    @Published var currentData: Data = Data()
+    private var page = 1
+
+    private var subscriptions: Set<AnyCancellable> = []
+    private var description: APIDescription
+
+    init(description: APIDescription) {
+        self.description = description
+    }
+
+    func read() {
+        fetch(url: description.url)
+            .sink(receiveCompletion: { completion in
+                switch completion {
+                case .finished:
+                    break
+                case .failure(let error):
+                    print(error.localizedDescription)
+                }
+            }, receiveValue: { data in
+                self.currentData = data
+            })
+            .store(in: &subscriptions)
+    }
+
+    private func fetch(url: String) -> AnyPublisher<Data, Error> {
+        page += 1
+        let finishedUrl = url.appending("?page=\(page)")
+        let request = URLRequest(url: URL(string: finishedUrl)!)
+
+        return URLSession.DataTaskPublisher(request: request, session: .shared)
+            .tryMap { data, response in
+                guard let response = response as? HTTPURLResponse, 200..<300 ~= response.statusCode else {
+                    #warning("ToDo: Error Handling")
+                    return data
+                }
+                return data
+            }
+            .eraseToAnyPublisher()
+    }
+}
