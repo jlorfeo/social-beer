87c6df41ca1c931f5b39ef46e7dcf5218c710cbb
Jan-Erik Lorfeo <lorfeo@adesso-mobile.de>
2020-09-12 21:28:46 +0200

    api-connect: Implement viewModel that listenes to the beers

--- SocialBeer/SocialBeer/ViewModel/BeerListViewModel.swift
+++ SocialBeer/SocialBeer/ViewModel/BeerListViewModel.swift
@@ -6,19 +6,58 @@
 //  Copyright © 2020 Lorfeo, Jan-Erik. All rights reserved.
 //
 
+import Combine
 import Foundation
 
-struct BeerListViewModel {
-    var beers: [Beer]
+class BeerListViewModel: ObservableObject {
+    @Published var beers: [Beer] = []
+
+    private var subscriptions: Set<AnyCancellable> = []
+    var beerInteractor: BeerService
+
+    init(beerInteractor: BeerService) {
+        self.beerInteractor = beerInteractor
+        beerInteractor.beerPublisher
+            .sink(receiveCompletion: { completion in
+                switch completion {
+                case .finished:
+                    break
+                case .failure(let error):
+                    guard let error = error as? DecodingError else {
+                        return
+                    }
+                    switch error {
+                    case .dataCorrupted(let error):
+                        print(error.debugDescription)
+                    case .keyNotFound(_, let context):
+                        print(context.debugDescription)
+                    case .typeMismatch(_ , let context):
+                        print(context.debugDescription)
+                    case .valueNotFound(_, let context):
+                        print(context.debugDescription)
+
+                    default:
+                        return
+                    }
+                }
+            }, receiveValue: { beers in
+                self.beers.append(contentsOf: beers)
+            })
+            .store(in: &subscriptions)
+    }
+
+    func fetchBeers() {
+        beerInteractor.fetchBeers()
+    }
 }
 
 struct Beer: Codable, Hashable {
     let id: Int
     let name: String
-    let description: String
     let first_brewed: String
+    let description: String
     let image_url: String
-    let abv: Double
-    let ph: Double
-    let ibu: Double
+    let abv: Double?
+    let ibu: Double?
+    let ph: Double?
 }
